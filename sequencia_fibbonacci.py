9a85d69b62cfaec1e76e8955b55abd43ea43dca5
Thomaz Andrey Walicoski <thomazandreywalicoski@gmail.com>
2024-08-09 20:21:32 -0300

    Sequência de Finnonacci

---
+++ sequencia_fibbonacci.py
@@ -0,0 +1,17 @@
+num_escolhido = int(input("Digite a posição do número na sequência de Fibbonacci que deseja ver: "))
+
+num_antecessor = 0
+num_atual = 0
+ordem = 0
+
+for i in range(0, num_escolhido):
+    num_atual = num_antecessor + num_atual
+    num_antecessor = num_atual - num_antecessor
+    ordem = ordem + 1
+    if(num_atual == 0):
+        num_atual = num_atual + 1
+    print(f"{ordem}° número: {num_atual}")
+
+print(f"O {num_escolhido}° número é o {num_atual}")
+    
+    
