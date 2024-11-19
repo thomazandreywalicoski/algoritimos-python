880de830da66ade06cf91216caed3d04ea21bbce
thomazandreywalicoski <thomazgithub@gmail.com>
2024-11-19 20:05:44 -0300

    Herança Multipla

--- POO/heranca_multipla.py
+++ POO/heranca_multipla.py
@@ -2,22 +2,45 @@ class Animal:
     def __init__(self, nome):
         self.nome = nome
 
-    def falar(self):
+    def pode_falar(self):
         print(f"{self.nome} faz um som.")
 
 class Voar:
     def pode_voar(self):
         print("Esse animal pode voar.")
 
-class Pato(Animal, Voar):
+class Botar:
+    def pode_botar(self):
+        print("Esse animal pode botar.")
+
+class Nadar:
+    def pode_nadar(self):
+        print("Esse animal pode nadar.")
+
+class Pato(Animal, Voar, Botar, Nadar):
     def __init__(self, nome):
         Animal.__init__(self, nome) # Inicializa a clase base Animal
 
-    def nadar(self):
-        print(f"{self.nome} está nadando!")
+class Galinha(Animal, Botar):
+    def __init__(self, nome):
+        Animal.__init__(self, nome) # Inicializa a clase base Animal
+
+class Ornitorinco(Animal, Voar, Botar, Nadar):
+    def __init__(self, nome):
+        Animal.__init__(self, nome) # Inicializa a clase base Animal
 
 if __name__ == "__main__":
     pato = Pato("Donalt")
-    pato.falar()
+    pato.pode_falar()
     pato.pode_voar()
-    pato.nadar()
+    pato.pode_nadar()
+    pato.pode_botar()
+
+    galinha = Galinha("Pepi")
+    galinha.pode_falar()
+    galinha.pode_botar()
+
+    ornitorinco = Ornitorinco("Blue")
+    ornitorinco.pode_falar()
+    ornitorinco.pode_botar()
+    ornitorinco.pode_nadar()
